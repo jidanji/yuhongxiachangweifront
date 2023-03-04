48cbd623400ae1deb035b49c3e895d5651eb490d
Maliang.China <42198953+jidanji@users.noreply.github.com>
2023-03-04 23:03:22 +0800

    更改跨域配置

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -2,7 +2,7 @@ import env from './env';
 
 const apiBaseUrlBase = {
   prod: 'http://139.224.193.145:7744',
-  development: '/api',
+  development: 'http://localhost:51055',
 };
 
 export const apiBaseUrl = apiBaseUrlBase[env];
